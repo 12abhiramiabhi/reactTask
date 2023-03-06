e98f34e3930cc7f269554b376e4bcb1ef59e4714
abhirami <abhabhirami972@gmail.com>
2023-03-05 22:30:47 -0800

    completed edit page

--- src/Components/Edit.jsx
+++ src/Components/Edit.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useLocation } from 'react-router-dom';
 
 
 
 function Edit() {
+
+    const location = useLocation()
+    console.log(location.state);
+
+    const [title,setTitle]=useState("")
+    const [discription, setDiscription]=useState("")
+
+
+    async function editBtn(){
+
+        let obj={title, discription}
+        let response=await axios.patch("localhost:3000/api/task/updatetask",obj)
+
+        console.log(response);
+    }
+
+
+    
+    
+
     return (
         <div>
             <div className="center">
                 <h1>edit your task</h1>
                 <div className="form1">
                     <div className="txt_field">
-                        <input type="text" className='title' required />
+                        <input type="text" value={title} className='title' required />
                         <span></span>
                         <label htmlFor="">Title</label>
                     </div>
@@ -18,7 +40,7 @@ function Edit() {
                         <span></span>
                         <label htmlFor="">Discription</label>
                     </div>
-                    <input type="submit" value="Submit" className='sub' />
+                    <input type="submit" onClick={editBtn} value="Submit" className='sub' />
                     <p className='parasss' >“Before you reject a task as Impossible, ask yourself why God presented you with this thing to be done.”</p>
                 </div>
             </div>
